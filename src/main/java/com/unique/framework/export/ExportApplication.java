65e68060d54dd2c787300580437607f7b9ed0ed5
haohaounique <haohaounique@163.com>
2025-03-25 23:32:52 +0800

    first commit

--- src/main/java/com/unique/framework/export/ExportApplication.java
+++ src/main/java/com/unique/framework/export/ExportApplication.java
@@ -1,8 +1,21 @@
 package com.unique.framework.export;
 
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
 /**
-* date:2025/3/25 21:50
-* author: haohaounique@163.com
-*/
+ * date:2025/3/25 21:50
+ * author: haohaounique@163.com
+ */
+@SpringBootApplication(scanBasePackages = "com.unique")
+@MapperScan(value = "com.unique.framework.export.mapper")
+@EnableFeignClients
+@EnableDiscoveryClient
 public class ExportApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ExportApplication.class, args);
+    }
 }
